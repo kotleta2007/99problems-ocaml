51e225e7c88365e90464b537502c50328c8b00fe
Mark <kotleta2007@protonmail.ch>
2023-09-20 12:16:51 +0200

    Added problems 16, 18, 19

--- solutions/pending/prob16.ml
+++ solutions/pending/prob16.ml
@@ -1,15 +1,14 @@
-(* let rec drop l n = drop_counter l n 1;; *)
-(* Error: Unbound value drop_counter *)
 let rec drop_counter l n counter = 
-match l with
-| [] -> []
-| h :: t -> 
-if n = counter
-then drop_counter t n 1
-else h :: (drop_counter t n (counter + 1));;
-(* val drop_counter : 'a list -> int -> int -> 'a list =
-  <fun> *)
+	match l with
+	| [] -> []
+	| h :: t -> 
+		if n = counter
+		then drop_counter t n 1
+		else h :: (drop_counter t n (counter + 1));;
 let drop l n = drop_counter l n 1;;
-(* val drop : 'a list -> int -> 'a list = <fun> *)
+
+(* TEST CASES:
+
 drop ["a"; "b"; "c"; "d"; "e"; "f"; "g"; "h"; "i"; "j"] 3;;
-(* - : string list = ["a"; "b"; "d"; "e"; "g"; "h"; "j"] *)
+
+*)
--- solutions/pending/prob19.ml
+++ solutions/pending/prob19.ml
@@ -6,13 +6,16 @@ let rec split_tail l1 l2 n =
                 | _ -> split_tail (l1 @ [h]) t (n - 1);;
 let split l n = split_tail [] l n;;
 
-let slice l i k =
-	let (end_removed, _) = split l (k + 1) in
-	let (_, begin_removed) = split end_removed i in
-	begin_removed;;
+let rec rotate l n =
+	let (left, right) = 
+		if n >= 0 
+		then (split l n)
+		else (split l (List.length l + n)) 
+	in (right @ left);;
 
 (* TEST CASES:
 
-slice ["a"; "b"; "c"; "d"; "e"; "f"; "g"; "h"; "i"; "j"] 2 6;;
+rotate ["a"; "b"; "c"; "d"; "e"; "f"; "g"; "h"] 3;;
+rotate ["a"; "b"; "c"; "d"; "e"; "f"; "g"; "h"] (-2);;
 
 *)
