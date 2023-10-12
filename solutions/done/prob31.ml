747deecef09bc97f249e479fa0e64c264a21d6b1
Mark <kotleta2007@protonmail.ch>
2023-10-12 16:02:00 +0200

    Added problem 31

---
+++ solutions/done/prob31.ml
@@ -0,0 +1,22 @@
+let is_prime n = 
+	let int_sqrt n = 
+		int_of_float 
+		(Float.floor 
+		(Float.sqrt 
+		(float_of_int n)))
+	and range a b = 
+		if a <= b then a :: range (a + 1) b 
+		else [] in
+	if n = 1 then false 
+	else List.fold_left 
+		(fun acc k -> acc && (n mod k != 0)) 
+		true 
+		(range 2 (int_sqrt n));;
+
+(* TEST CASES:
+
+not (is_prime 1);;
+is_prime 7;;
+not (is_prime 12);;
+
+*)
