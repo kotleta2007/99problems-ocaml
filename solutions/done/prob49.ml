66a0a01f5a8479558bbcdcf9d20a8ff5e8b7bac0
Mark <kotleta2007@protonmail.ch>
2023-10-23 18:49:57 +0200

    Cleaner solution for 49: less anonymous functions

--- solutions/done/prob49.ml
+++ solutions/done/prob49.ml
@@ -8,8 +8,9 @@ let rec gray n =
 	if n = 1 then ["0"; "1"]
 	else 
 		let prev = gray (n - 1) in
-		let zero = List.map (fun x -> "0" ^ x) prev in
-		let one  = List.map (fun x -> "1" ^ x) (rev prev) in
+		let prepend_with s = fun x -> s ^ x in
+		let zero = List.map (prepend_with "0") prev in
+		let one  = List.map (prepend_with "1") (rev prev) in
 	zero @ one;;
 
 (* TEST CASES:
